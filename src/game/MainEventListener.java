1e55e028337242abe1e55ca2551d7704de2ade09
joetifa2003 <joetifa2003@gmail.com>
2023-11-30 04:42:15 +0200

    feat: POC of hovering on column

--- src/game/MainEventListener.java
+++ src/game/MainEventListener.java
@@ -5,6 +5,10 @@ import helper.Vector;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Optional;
 
 enum CellState {
     EMPTY,
@@ -12,8 +16,10 @@ enum CellState {
     YELLOW
 }
 
-public class MainEventListener implements GLEventListener {
+public class MainEventListener implements GLEventListener, MouseMotionListener, MouseListener {
     CellState[][] state = new CellState[6][7];
+
+    Optional<Integer> hoveredOnColumn = Optional.empty();
 
     final double CELL_SIZE = 80;
 
@@ -35,10 +41,69 @@ public class MainEventListener implements GLEventListener {
 
         for (int y = 0; y < state.length; y++) {
             final CellState[] row = state[y];
+
+
             for (int x = 0; x < row.length; x++) {
+
+                gl.glColor3d(1, 1, 1);
+                if (hoveredOnColumn.orElse(-1) == x) {
+                    gl.glColor3d(1, 1, 0);
+                    drawTri(gl, new Vector( x*CELL_SIZE + 4.5 * CELL_SIZE, 1 * CELL_SIZE + 6.5 * CELL_SIZE), CELL_SIZE);
+                }
+
                 drawRect(gl, new Vector(x * CELL_SIZE + 4.5 * CELL_SIZE, y * CELL_SIZE + 1.5 * CELL_SIZE), CELL_SIZE);
+                gl.glColor3d(1, 1, 1);
+
             }
         }
+    }
+
+    public void drawRect(GL gl, Vector pos, double size) {
+        gl.glPushMatrix();
+        gl.glTranslated(pos.x(), pos.y(), 0);
+            gl.glBegin(GL.GL_LINE_LOOP);
+                gl.glVertex2d(0, 0);
+                gl.glVertex2d(size, 0);
+                gl.glVertex2d(size, size);
+                gl.glVertex2d(0, size);
+            gl.glEnd();
+        gl.glPopMatrix();
+    }
+
+
+    public void drawTri(GL gl, Vector pos, double size) {
+        gl.glPushMatrix();
+        gl.glTranslated(pos.x(), pos.y(), 0);
+        gl.glBegin(GL.GL_TRIANGLES);
+        gl.glVertex2d(0, size);
+        gl.glVertex2d(size, size);
+        gl.glVertex2d(size/2, 5);
+        gl.glEnd();
+        gl.glPopMatrix();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        Vector mp = new Vector(e);
+        mp = mp.setY(e.getComponent().getHeight() - mp.y()); // transform screen to world cords
+
+        this.hoveredOnColumn = Optional.empty();
+        for (int x = 0; x < 7; x++) {
+            double cellX = x * CELL_SIZE + 4.5 * CELL_SIZE;
+            if (mp.x() < cellX + CELL_SIZE && mp.x() > cellX) {
+                this.hoveredOnColumn = Optional.of(x);
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
     }
 
     @Override
@@ -51,15 +116,23 @@ public class MainEventListener implements GLEventListener {
 
     }
 
-    public void drawRect(GL gl, Vector pos, double size) {
-        gl.glPushMatrix();
-        gl.glTranslated(pos.x(), pos.y(), 0);
-        gl.glBegin(GL.GL_LINE_LOOP);
-            gl.glVertex2d(0, 0);
-            gl.glVertex2d(size, 0);
-            gl.glVertex2d(size, size);
-            gl.glVertex2d(0, size);
-        gl.glEnd();
-        gl.glPopMatrix();
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
